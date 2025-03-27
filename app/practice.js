c0765525db0091ddccca30d7f7e3cd1726104d59
VaishnaviSSWW <vaishnavi.ssww@gmail.com>
2025-03-27 17:07:55 +0530

    new file added

---
+++ app/practice.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const practice = () => {
+  return (
+    <View>
+      <Text>practice</Text>
+    </View>
+  )
+}
+
+export default practice
+
+const styles = StyleSheet.create({})
